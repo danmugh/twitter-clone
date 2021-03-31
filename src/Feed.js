7f87dce79d941d7f75543ab99d205495941f6ada
Dan Mugisho <danmmwx@gmail.com>
2021-03-31 21:35:22 +0300

    Complete the navbar

--- src/Feed.js
+++ src/Feed.js
@@ -4,10 +4,23 @@ import TweetBox from "./TweetBox";
 import Post from "./Post";
 import db from "./firebase";
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+import SidebarMenu from "./SidebarMenu";
 
 function Feed() {
 
     const [posts, setPosts] = useState([]);
+    const [menu, setMenu] = useState(false);
+    const [backdrop, setBackdrop] = useState(false);
+
+    const handleMenu = () => {
+        setMenu(!menu)
+        setBackdrop(!backdrop)
+    };
+
+    const handleCloseBackdrop = () => {
+        handleMenu()
+    };
 
     useEffect(() => {
         db.collection("posts").onSnapshot((snapshot) =>
@@ -18,14 +31,22 @@ function Feed() {
     return (
         <div className="feed">
             <div className="navbar">
-                <div className="feed__header">
                 <span className="menu" >
-                    <MenuIcon className="menu__icon" style={{ fontSize: '30px' }} />
+                        <MenuIcon
+                            onClick={ handleMenu }
+                            className="menu__icon"
+                            style={{ fontSize: '30px' }} />
                 </span>
-                    <h2>Home</h2>
-                </div>
+                <h2>Home</h2>
             </div>
+            <div className="adjustment" style={{ paddingBottom: '85px' }} />
 
+            {/*<div className="feed__header">*/}
+            {/*        <span className="menu" >*/}
+            {/*            <MenuIcon className="menu__icon" style={{ fontSize: '30px' }} />*/}
+            {/*        </span>*/}
+            {/*    <h2>Home</h2>*/}
+            {/*</div>*/}
 
             <TweetBox />
 
@@ -59,6 +80,26 @@ function Feed() {
             {/*<Post />*/}
             {/*<Post />*/}
             {/*<Post />*/}
+
+            <div>
+                <nav className={menu ? 'nav-menu active' : 'nav-menu'}>
+                    <ul className='nav-menu-items'  >
+                        <li className='navbar-toggle'>
+                            <a href="#">
+                                <CloseIcon onClick={handleMenu}
+                                           className='close-icon'
+                                           style={{ color: 'var(--twitter-color)', fontSize: '35px'}} />
+                            </a>
+                        </li>
+
+                        <SidebarMenu />
+                    </ul>
+                </nav>
+            </div>
+            <div
+                onClick={handleCloseBackdrop}
+                className={ backdrop ? "backdrop" : "" }
+            />
 
         </div>
     );
